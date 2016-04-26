58b88b88d9a666f67c6ed9b69434fb0424854063
Tiago <tiagoft@gmail.com>
2016-04-26 15:20:33 -0300

    [master] Changed table creation script to avoid re-creating tables

--- python/create_tables.py
+++ python/create_tables.py
@@ -13,13 +13,34 @@ if not os.path.exists(constants.dbdir):
 
 conn = sqlite3.connect(constants.dbfile)
 c = conn.cursor()
-c.execute('''CREATE TABLE media
-                     (id int,
+c.execute('''CREATE TABLE IF NOT EXISTS media
+                     (id int PRIMARY KEY,
                      title text,
+                     artist text,
+                     place_original text,
+                     place_performer text,
+                     year_original int,
+                     year_perfomance int,
                      comments text,
                      link text,
+                     user text,
                      file text)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS features
+                     (id int PRIMARY KEY,
+                     media_id int,
+                     algorithm text,
+                     metadata text,
+                     value text)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS users
+                    (username text PRIMARY KEY,
+                    password text)''')
+
 conn.commit()
 
 
+for table in ['media', 'features', 'users']:
+    results = c.execute('''SELECT * FROM ''' + table)
+    print "TABLE", table, "has", len(list(results)), "lines"
 
