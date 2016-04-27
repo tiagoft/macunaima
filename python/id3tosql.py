444eccffbcc9fa08d263fc852dabd00c9e312b20
Tiago <tiagoft@gmail.com>
2016-04-27 08:55:42 -0300

    [master] Add app to insert values into sql database using ID3 tags

---
+++ python/id3tosql.py
@@ -0,0 +1,70 @@
+
+import eyed3
+import sqlite3
+import os
+import uuid
+
+import constants
+
+
+
+
+def id3tosql(mp3_filename, dbfile):
+    metadata = eyed3.load(mp3_filename).tag
+    new_id = uuid.uuid4().int
+
+    if metadata.genre.id == 255:
+        genre = "None"
+    else:
+        genre = metadata.genre.name
+
+    if metadata.original_release_date is None:
+        original_release_date = 0
+    else:
+        original_release_date = metadata.original_release_date
+
+    if metadata.recording_date is None:
+        recording_date = 0
+    else:
+        recording_date = metadata.recording_date
+
+
+
+    print str(new_id)
+    print metadata.title
+    print metadata.artist
+    print metadata.album
+    print metadata.recording_date
+    print metadata.original_release_date
+    print genre
+    print mp3_filename
+
+    query = "INSERT INTO media (id, title, artist, album, recording_date, "+\
+                "original_release_date, genre, file, user) VALUES ('" +\
+                            str(new_id) + "', '" +\
+                            unicode(metadata.title) + "', '" +\
+                            unicode(metadata.artist) + "', '" +\
+                            unicode(metadata.album) + "', " +\
+                            unicode(recording_date) + ", " +\
+                            unicode(original_release_date) + ", '" +\
+                            genre + "', '" +\
+                            mp3_filename + "', '" +\
+                            "admin');"
+    print query
+
+
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    print sys.argv[1]
+    print sys.argv[2]
+    id3tosql(sys.argv[1], sys.argv[2])
+
